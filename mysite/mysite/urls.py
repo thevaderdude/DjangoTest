0e80736986295660eea14a3b32c216915757e3e5
thevaderdude <valen@potloff.com>
2019-11-20 09:38:04 -0800

    made some stupid polls app

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+
+urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
