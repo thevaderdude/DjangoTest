0f66c937656a97de481d7b8367b6feddc8ed1664
thevaderdude <valen@potloff.com>
2019-11-23 21:51:33 -0800

    added some of the stupid polls website thing

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,10 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
